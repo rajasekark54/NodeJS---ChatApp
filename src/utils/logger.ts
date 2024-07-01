2e6c8cfe7a6dc92abdf596b20fcaf7c3c90f398b
Rajasekar <rajasekark54@gmail.com>
2024-07-01 10:51:31 +0530

    logger implementation done

---
+++ src/utils/logger.ts
@@ -0,0 +1,21 @@
+import { createLogger, format, transports, Logger } from 'winston';
+const { combine, timestamp, printf, colorize } = format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger: Logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    colorize(),
+    logFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+export default logger;
